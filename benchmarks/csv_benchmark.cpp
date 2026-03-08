e2d0ab0e6aa5145735ecf62c523321abc519da0b
FastParseX-dev <fastparsex@gmail.com>
2026-03-08 15:22:02 +0100

    Update csv_benchmark.cpp

--- benchmarks/csv_benchmark.cpp
+++ benchmarks/csv_benchmark.cpp
@@ -1,16 +1,21 @@
+#include <fastparsex/fastparsex.hpp>
 #include <chrono>
 #include <iostream>
 
 int main() {
-    std::cout << "CSV benchmark placeholder" << std::endl;
-
     auto start = std::chrono::high_resolution_clock::now();
 
-    // TODO: integrate FastParseX CSV parser here
+    fp::CSVParser parser("data.csv");
+    fp::RowView row;
+
+    size_t count = 0;
+    while (parser.next_row(row)) {
+        count++;
+    }
 
     auto end = std::chrono::high_resolution_clock::now();
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
-    std::cout << "Elapsed: " << ms << " ms" << std::endl;
-    return 0;
+    std::cout << "Parsed rows: " << count << "\n";
+    std::cout << "Time: " << ms << " ms\n";
 }
