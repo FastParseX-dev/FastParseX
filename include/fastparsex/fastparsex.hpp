1ee059de14a005c35b30fe0434885bdec6dd0ff9
FastParseX-dev <fastparsex@gmail.com>
2026-03-08 22:14:07 +0100

    Create fastparsex.hpp

---
+++ include/fastparsex/fastparsex.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "fastparsex/version.hpp"
+#include "fastparsex/types.hpp"
+#include "fastparsex/error.hpp"
+#include "fastparsex/file_reader.hpp"
+
+#include "fastparsex/csv/csv_parser.hpp"
+#include "fastparsex/csv/csv_reader.hpp"
+#include "fastparsex/csv/config.hpp"
+
+#include "fastparsex/binary/binary_parser.hpp"
+#include "fastparsex/binary/binary_reader.hpp"
+
+#include "fastparsex/log/log_config.hpp"
+#include "fastparsex/log/log_parser.hpp"
+#include "fastparsex/log/log_reader.hpp"
+
+#include "fastparsex/parallel/parallel_csv.hpp"
+#include "fastparsex/parallel/parallel_log.hpp"
+
+#include "fastparsex/profiling/profiler.hpp"
+#include "fastparsex/export/arrow_writer.hpp"
