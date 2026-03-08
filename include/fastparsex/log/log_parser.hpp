3139c8edc55be02ba527ec7328aaf7976ad7071e
FastParseX-dev <fastparsex@gmail.com>
2026-03-08 22:21:09 +0100

    Update log_parser.hpp

--- include/fastparsex/log/log_parser.hpp
+++ include/fastparsex/log/log_parser.hpp
@@ -1,1 +1,27 @@
+#pragma once
+
+#include <string>
+#include <functional>
+#include <vector>
+#include "fastparsex/log/log_config.hpp"
+
+namespace fp {
+
+class LogParser {
+public:
+    LogParser(const std::string& path, LogConfig config = apache_common());
+
+    // Avvia il parsing del file
+    void parse();
+
+    // Callback per ogni record parsato
+    void on_record(std::function<void(const std::vector<std::string>&)> cb);
+
+private:
+    std::string path_;
+    LogConfig config_;
+    std::function<void(const std::vector<std::string>&)> callback_;
+};
+
+} // namespace fp
 
