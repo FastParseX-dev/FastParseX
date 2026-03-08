e9534182da16dc157d9bc43e3699b6a4ca0e5fe2
FastParseX-dev <fastparsex@gmail.com>
2026-03-08 22:29:26 +0100

    Create parallel_log.cpp

---
+++ src/parallel/parallel_log.cpp
@@ -0,0 +1,13 @@
+#include "fastparsex/parallel/parallel_log.hpp"
+#include <thread>
+
+namespace fp {
+
+ParallelLogParser::ParallelLogParser(const std::string& path)
+    : path_(path) {}
+
+void ParallelLogParser::parse_parallel() {
+    // Placeholder semplice
+}
+
+} // namespace fp
