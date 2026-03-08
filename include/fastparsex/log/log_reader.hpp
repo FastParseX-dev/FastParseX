dfacece5afbbd455d4e06705f38e271a390d80fb
FastParseX-dev <fastparsex@gmail.com>
2026-03-08 22:21:22 +0100

    Update log_reader.hpp

--- include/fastparsex/log/log_reader.hpp
+++ include/fastparsex/log/log_reader.hpp
@@ -1,1 +1,22 @@
 
+#pragma once
+
+#include <string>
+#include <cstdio>
+
+namespace fp {
+
+class LogReader {
+public:
+    explicit LogReader(const std::string& path);
+    ~LogReader();
+
+    // Legge la prossima linea dal file
+    bool next_line(std::string& out);
+
+private:
+    std::string path_;
+    FILE* file_ = nullptr;
+};
+
+} // namespace fp
